ba54425447354bdd15ac329c620f9e42a97b5e96
dengzemiao <dengzemiaovip@163.com>
2025-07-03 11:23:14 +0800

    2025-07-03 11:23:09 提交优化

--- lib/model/a_case.dart
+++ lib/model/a_case.dart
@@ -9,12 +9,6 @@ class CaseModel {
     required this.id,
   });
 
-  /// 从原始 JSON 转换
-  factory CaseModel.fromRawJson(String str) => CaseModel.fromJson(json.decode(str));
-
-  /// 转换为原始 JSON
-  String toRawJson() => json.encode(toJson());
-
   /// 拷贝
   CaseModel copyWith({
     int? id,
@@ -31,6 +25,17 @@ class CaseModel {
     return model;
   }
 
+  /// 转换为 JSON
+  Map<String, dynamic> toJson() => {
+    "id": id,
+  };
+
+  /// 从原始 JSON 转换
+  factory CaseModel.fromRawJson(String str) => CaseModel.fromJson(json.decode(str));
+
+  /// 转换为原始 JSON
+  String toRawJson() => json.encode(toJson());
+
   /// 更新数据
   /// [force] 是否强制覆盖（即使为null也覆盖），默认为false，仅当 json 中有对应 key 时才覆盖
   void updateFromJson(Map<String, dynamic> json, {bool force = false}) {
@@ -45,9 +50,4 @@ class CaseModel {
     final jsonData = json.decode(str ?? '{}');
     updateFromJson(jsonData, force: force);
   }
-
-  /// 转换为 JSON
-  Map<String, dynamic> toJson() => {
-    "id": id,
-  };
 }
